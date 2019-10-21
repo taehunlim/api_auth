a899c32d4e81d8d970f3cbc0c6996675806086f7
taehunlim <dkslsdkdl@naver.com>
2019-10-21 17:09:28 +0900

    add signup

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const userModel = require('../models/user');
+
 // @@ route POST user/signup
 // @@ desc  user signup route
 // @@ access public
 router.post('/signup', async (req, res) => {
-   res.json({
-       msg : "work!"
-   });
+   const {username, email, password} = req.body;
+
+   const foundUser = await userModel.findOne({email});
+   if(foundUser) {
+    //user 가 잇다면
+       return res.status(403).json({
+          error : 'email is already in use'
+       });
+   }
+   else{
+       // user가 없다면
+       const newUser = new userModel({ username, email, password});
+       await newUser
+           .save()
+           .then(user => {
+               res.status(200).json(user);
+           })
+           .catch(err => res.json(err));
+   }
+
+
 });
 
 
